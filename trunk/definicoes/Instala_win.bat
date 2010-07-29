f1db08c881d467e1c734eaed1f69e9c31d09de4d
ahdrein <ahdrein@3aeb3564-334f-a6c8-d04b-efc318c5705c>
2010-07-29 18:46:00 +0000

    Mudanças do novo Layout

--- trunk/definicoes/Instala_win.bat
+++ trunk/definicoes/Instala_win.bat
@@ -1,2 +1,3 @@
 set nls_lang=AMERICAN_AMERICA.WE8ISO8859P1
 sqlplus /nolog @.\dispara_script_instalacao.sql
+del arquivo_temporario.sql       
--- trunk/definicoes/processa_grants.sql
+++ trunk/definicoes/processa_grants.sql
@@ -1,7 +1,28 @@
 @".\define.sql"
 conn &&INOUT_USER/&&INOUT_PASS@&&TNS
-exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER');
+exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER', p_tipo => 'R');
 
 conn &&INTEGRACAO_USER/&&INTEGRACAO_PASS@&&TNS
-exec sfw_processa_grants(p_owner => '&&INOUT_USER');
+exec sfw_processa_grants(p_owner => '&&INOUT_USER', p_tipo => 'R');
+
+conn &&SOFT_USER/&&SOFT_USER@&&TNS
+exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER', p_tipo => 'R');
+
+conn &&IMPORT_USER/&&IMPORT_USER@&&TNS
+exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER', p_tipo => 'R');
+
+conn &&BROKER_USER/&&BROKER_USER@&&TNS
+exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER', p_tipo => 'R');
+
+conn &&EXPORT_USER/&&EXPORT_USER@&&TNS
+exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER', p_tipo => 'R');
+
+conn &&DRAWBACK_USER/&&DRAWBACK_USER@&&TNS
+exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER', p_tipo => 'R');
+
+conn &&CAMBIO_EXP_USER/&&CAMBIO_EXP_USER@&&TNS
+exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER', p_tipo => 'R');
+
+conn &&CAMBIO_IMP_USER/&&CAMBIO_IMP_USER@&&TNS
+exec sfw_processa_grants(p_owner => '&&INTEGRACAO_USER', p_tipo => 'R');
 
